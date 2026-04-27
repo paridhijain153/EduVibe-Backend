fbdf6c6bbeff0cb4adb802cdff1d3db0c13bb54b
Paridhi Jain <paridhijain153@gmail.com>
2026-04-27 07:51:53 +0530

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5055;
+
 module.exports = {
-  PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGODB_URI || process.env.MONGO_URI || process.env['process.env.MONGO_URI'],
+  PORT,
+  MONGO_URI: process.env.MONGODB_URI || process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET || 'this is our project secret key',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '7d'
 };
